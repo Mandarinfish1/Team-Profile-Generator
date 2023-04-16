d14221b36fba93df9f6518e40dd09293a564e0eb
Mandarinfish <66844643+Mandarinfish1@users.noreply.github.com>
2023-04-15 23:50:46 -0400

    updating .gitignore/index.js/package.json

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const inquirer = require('inquirer');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
-const generateHTML = require('./src/generateHTML');
+/*const generateHTML = require('./src/generateHTML');*/
 const path = require('path');
 const fs = require('fs');
 
